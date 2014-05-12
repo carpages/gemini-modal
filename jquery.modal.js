e9bcaf4b63c23a1617ca54cf9c75c2cdccafff1d
Matt Rose <mrose@carpages.ca>
2014-05-12 16:26:35 -0400

    GEMJS-46: Make templates extendable

--- jquery.modal.js
+++ jquery.modal.js
@@ -1,112 +1,115 @@
 define(['jquery.boiler', 'modules/modal/templates'], function($, T){
 
-	//Make an object to be used by both $.modal and $.fn.modal
-	$.Modal = function(options){
-		
-		var plugin = {
-			settings: $.extend({}, {
-				content: '',
-				template: 'modalDefault',
-				modal: '.modal',
-				modalContent: '.modal__content',
-				exit: '.modal__close',
-				onOpen: false,
-				onClose: false,
-				fixed: false,
-				stopPropagation: false
-			}, options),
-			init: function(){
-				var plugin = this;
+  //Make an object to be used by both $.modal and $.fn.modal
+  $.Modal = function(options){
 
-				//Cache wrapper, modal, and exit
-				plugin.$wrapper = $(T[plugin.settings.template]())._hide();
-				plugin.$modal = plugin.$wrapper.find(plugin.settings.modal);
-				plugin.$content = plugin.$wrapper.find(plugin.settings.modalContent);
-				plugin.$exit = plugin.$wrapper.find(plugin.settings.exit);
+    var plugin = {
+      settings: $.extend({}, {
+        content: '',
+        modal: '.modal',
+        modalContent: '.modal__content',
+        exit: '.modal__close',
+        onOpen: false,
+        onClose: false,
+        fixed: false,
+        stopPropagation: false,
+        templates: {},
+      }, options),
+      init: function(){
+        var plugin = this;
 
-				//Add content
-				plugin.$content.html(plugin.settings.content);
+        //Extend the templates
+        plugin.T = $.extend(T, plugin.settings.templates);
 
-				//Append modal to body
-				$('body').append(plugin.$wrapper);
+        //Cache wrapper, modal, and exit
+        plugin.$wrapper = $(plugin.T['modal']())._hide();
+        plugin.$modal = plugin.$wrapper.find(plugin.settings.modal);
+        plugin.$content = plugin.$wrapper.find(plugin.settings.modalContent);
+        plugin.$exit = plugin.$wrapper.find(plugin.settings.exit);
 
-				//Close event on wrapper click and exit click
-				var $stop;
-				if(plugin.settings.stopPropagation){
-					$stop = plugin.$wrapper.find(plugin.settings.stopPropagation);
-				} else{
-					$stop = plugin.$modal;
-				}
+        //Add content
+        plugin.$content.html(plugin.settings.content);
 
-				var stop = false;
-				plugin.$wrapper.click(function(){
-					if(stop){
-						stop = false;
-					} else {
-						plugin.close();
-					}
-				});
-				$stop.click(function(e){
-					stop = true;
-				});
-				plugin.$exit.click(function(){
-					plugin.close();
-				});
-			},
-			open: function(){
-				var plugin = this;
+        //Append modal to body
+        $('body').append(plugin.$wrapper);
 
-				//Calculate top
-				if(!plugin.settings.fixed){
-					var top = ($(window).height() - plugin.$modal.height()) / 2;
-					top = Math.max(top, 0);
-					plugin.$modal.css('top', $(window).scrollTop() + top);
-				}
+        //Close event on wrapper click and exit click
+        var $stop;
+        if(plugin.settings.stopPropagation){
+          $stop = plugin.$wrapper.find(plugin.settings.stopPropagation);
+        } else{
+          $stop = plugin.$modal;
+        }
+
+        var stop = false;
+        plugin.$wrapper.click(function(){
+          if(stop){
+            stop = false;
+          } else {
+            plugin.close();
+          }
+        });
+        $stop.click(function(e){
+          stop = true;
+        });
+        plugin.$exit.click(function(){
+          plugin.close();
+        });
+      },
+      open: function(){
+        var plugin = this;
+
+        //Calculate top
+        if(!plugin.settings.fixed){
+          var top = ($(window).height() - plugin.$modal.height()) / 2;
+          top = Math.max(top, 0);
+          plugin.$modal.css('top', $(window).scrollTop() + top);
+        }
 
 
-				plugin.$wrapper.addClass('is-active')._fadeIn(250);
+        plugin.$wrapper.addClass('is-active')._fadeIn(250);
 
-				if(plugin.settings.onOpen) plugin.settings.onOpen.call(plugin);
-			},
-			close: function(){
-				var plugin = this;
+        if(plugin.settings.onOpen) plugin.settings.onOpen.call(plugin);
+      },
+      close: function(){
+        var plugin = this;
 
-				plugin.$wrapper.removeClass('is-active')._fadeOut(250);
+        plugin.$wrapper.removeClass('is-active')._fadeOut(250);
 
-				if(plugin.settings.onClose) plugin.settings.onClose.call(plugin);
-			},
-			update: function(content){
-				plugin.$content.html(content);
-			}
-		};
+        if(plugin.settings.onClose) plugin.settings.onClose.call(plugin);
+      },
+      update: function(content){
+        plugin.$content.html(content);
+      }
+    };
 
-		plugin.init();
+    plugin.init();
 
-		return plugin;
-	};
+    return plugin;
+  };
 
-	$.boiler('modal', {
-		defaults: {},
+  $.boiler('modal', {
+    defaults: {},
 
-		init: function(){
-			var plugin = this;
+    init: function(){
+      var plugin = this;
 
-			plugin.modal = new $.Modal({
-				content: plugin.$el.html()
-			});
-		},
+      plugin.modal = new $.Modal({
+        content: plugin.$el.html()
+      });
+    },
 
-		open: function(){
-			this.modal.open();
-		},
+    open: function(){
+      this.modal.open();
+    },
 
-		close: function(){
-			this.modal.close();
-		}
-	});
+    close: function(){
+      this.modal.close();
+    }
+  });
 
-	// Return the jquery object
-	// This way you don't need to require both jquery and the plugin
-	return $;
+  // Return the jquery object
+  // This way you don't need to require both jquery and the plugin
+  return $;
 
 });
